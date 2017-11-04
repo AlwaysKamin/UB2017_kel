ceec3b34af8c8e5527c5176d323f9463f7b441fa
elias <gaytone@sunyit.edu>
2017-11-04 15:53:29 -0400

    Start of Main, Button Grid

--- main.py
+++ main.py
@@ -1,49 +1,24 @@
-# from tkinter import *
-#
-# class Example(Frame):
-#
-#     def __init__(self, parent):
-#         Frame.__init__(self, parent, background="green")
-#
-#         self.parent = parent
-#
-#         self.parent.title("Window")
-#         self.pack(fill=BOTH, expand=1)
-#
-# # creates window
-# root = Tk()
-#
-# root.title("Apprentice")
-# root.geometry("900x800")
-#
-# # app = Frame(root)
-# # app.grid()
-# # button1 = Button(app, text="Scan Block")
-# # button.grid()
-#
-# # kicks off main root.
-# root.mainloop()
-
-
-
+import sys
+import ctypes
 from tkinter import *
+#from sdl2 import*
 class App:
   def __init__(self, master):
     frame = Frame(master)
     frame.pack()
     self.button = Button(frame,
-                         text="Scan Block", fg="red",
+                         text="Scan Block",
                          command=quit)
-    self.button.pack(side=LEFT)
+    self.button.grid(row=0, column=0)
     self.button2 = Button(frame,
                          text="Display on Block",
                          command=quit)
-    self.button2.pack(side=LEFT)
+    self.button2.grid(row=0, column=1)
 
-
-root = Tk()
-root.title("Apprentice")
-root.geometry("900x800")
-app = App(root)
-root.mainloop()
+def main():
+    root = Tk()
+    root.title("Apprentice")
+    root.geometry("900x800")
+    app = App(root)
+    root.mainloop()
 
