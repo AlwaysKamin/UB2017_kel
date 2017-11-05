7f5cac418ef8c15dc5b1b5f82d9dd9f8c3158eec
Kamin <kaminjfay@gmail.com>
2017-11-05 10:43:20 -0500

    Final for presentation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
 from tkinter import *
 
-# import cv2
-# from blockDetection import blockDetection
+import cv2
+from blockDetection import blockDetection
 from projection import projection
 from template import template2x4
 
@@ -11,52 +11,28 @@ class GUI:
     frame = Frame(master)
     master.title("Apprentice")
     frame.pack()
-    self.button = Button(frame,
-                         text="Quit",
-                         width=15,
-                         command=quit)
-    self.button.grid(row=0, column=1)
-    self.button2 = Button(frame,
-                         text="Display on Block",
-                         width=15,
-                         command=quit)
-    self.button2.grid(row=0, column=2)
-    self.button3 = Button(frame,
+
+    self.button0 = Button(frame,
+                          text="Recognize Block",
+                          width=15,
+                          command=lambda: cameraMechanics())
+    self.button0.grid(row = 0, column = 0)
+    self.button1 = Button(frame,
                           text="Choose Template 1",
                           width=15,
                           command=lambda: templateSelection(1))
-    self.button3.grid(row=1, column=0)
-
-    self.button4 = Button(frame,
+    self.button1.grid(row=1, column=0)
+    self.button2 = Button(frame,
                           text="Choose Template 2",
                           width=15,
                           command=lambda: templateSelection(2))
-    self.button4.grid(row=2, column=0)
+    self.button2.grid(row=2, column=0)
     self.button3 = Button(frame,
-                          text="Display on Block",
-                          width=15,
-                          command=quit)
+                         text="Quit",
+                         width=15,
+                         command=quit)
+    self.button3.grid(row=3, column=0)
 
-# def temp_one(temp_choice):
-#     x = [0, 0, 0, 0]
-#     y = [0, 0, 0, 0]
-#     if temp_choice == 1:
-#         # x1 = 175
-#         # y1 = 100
-#         # x2 = 2000
-#         # y2 = 100
-#         # x3 = 1000
-#         # y3 = 100
-#         # x4 = 1000
-#         # y4 = 250
-#         x = [175, 2000, 1000, 1000]
-#         y = [100, 100, 100, 250]
-#         print("Temp One Selected")
-#
-#
-#     if temp_choice == 2:
-#         x = [175, 2000, 1000, 1000]
-#         y = [100, 100, 100, 250]
 def templateSelection(choice):
     newTemplate = template2x4()
     if choice == 1:
@@ -82,15 +58,15 @@ def renderer(newTemplate2x4):
     testProjection.display()
 
 
-# def cameraMechanics():
-#     print("Entering camera Mechanics")
-#     camera = blockDetection()
-#     cameraFrame = camera.grabFrames()
-#     cameraBW = camera.toBW(cameraFrame)
-#     cameraThres = camera.threshold(cameraBW)
-#     cameraDisplay = camera.locateAndDrawContours(cameraThres, cameraFrame)
-#     # camera.display(cameraThres)
-#     camera.display(cameraDisplay)
+def cameraMechanics():
+    print("Entering camera Mechanics")
+    camera = blockDetection()
+    cameraFrame = camera.grabFrames()
+    cameraBW = camera.toBW(cameraFrame)
+    cameraThres = camera.threshold(cameraBW)
+    cameraDisplay = camera.locateAndDrawContours(cameraThres, cameraFrame)
+    # camera.display(cameraThres)
+    camera.display(cameraDisplay)
 
 def main():
     while True:
