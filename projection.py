87d7042668c12362ef9977aeff51a8b33b4b0884
Unknown <lukekrentz@gmail.com>
2017-11-04 21:22:33 -0500

    Removed "Main" part of the file

--- projection.py
+++ projection.py
@@ -12,18 +12,13 @@ class Render(Frame):
         self.pack(fill=BOTH, expand=1)
 
         canvas = Canvas(self)
-        canvas.create_line(15, 25, 200, 25)
-        canvas.create_line(15, 50, 200, 50)
+
+        canvas.create_line(15, 75, 400, 75)
 
         canvas.pack(fill=BOTH, expand=1)
 
 
-def main():
-    root = Tk()
-    ex = Render()
-    root.attributes('-fullscreen', True)
-    root.mainloop()
-
-
-if __name__ == '__main__':
-    main()
+root = Tk()
+ex = Render()
+root.attributes('-fullscreen', True)
+root.mainloop()
