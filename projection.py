8096e01540d6e785324896d3b0e4bbd903967167
Unknown <lukekrentz@gmail.com>
2017-11-04 20:05:08 -0400

    Created example lines

--- projection.py
+++ projection.py
@@ -1,10 +1,29 @@
-import tkinter as tk
+from tkinter import Tk, Canvas, Frame, BOTH
 
-class FullScreenApp(object):
+
+class Render(Frame):
     def __init__(self):
-        self.tk = tk()
-        return self.tk
+        super().__init__()
 
-root=tk.Tk()
-root.attributes('-fullscreen', True)
-root.mainloop()
+        self.initUI()
+
+    def initUI(self):
+        self.master.title("Lines")
+        self.pack(fill=BOTH, expand=1)
+
+        canvas = Canvas(self)
+        canvas.create_line(15, 25, 200, 25)
+        canvas.create_line(15, 50, 200, 50)
+
+        canvas.pack(fill=BOTH, expand=1)
+
+
+def main():
+    root = Tk()
+    ex = Render()
+    root.attributes('-fullscreen', True)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
